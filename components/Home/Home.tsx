1700a3e444d28fd86c9d889267c3ee0cf4f5d68a
rahulpra045 <98214910+rahulpra045@users.noreply.github.com>
2024-09-30 19:58:01 +0530

    Top Collection Card

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './Hero/Hero'
 import HotBids from './HotBids/HotBids'
+import TopCollection from './TopCollection/TopCollection'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <HotBids/>
+      <TopCollection/>
     </div>
   )
 }
