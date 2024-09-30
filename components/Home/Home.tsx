faa39b2a7cbdf2a83fa10915f02379f9d36a0107
rahulpra045 <98214910+rahulpra045@users.noreply.github.com>
2024-09-30 19:34:31 +0530

    Added HotBid section

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Hero/Hero'
+import HotBids from './HotBids/HotBids'
 
 const Home = () => {
   return (
     <div>
-      <Hero/>
+      <Hero />
+      <HotBids/>
     </div>
   )
 }
